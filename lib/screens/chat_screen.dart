e02fccc31dd97a4fe0b6719fcc982c0f6e6567c9
Sophia Castellucci <sophiacastellucci@mac.home>
2026-06-29 14:10:35 +0200

    chore: clean unused auth import from chat screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import '../services/auth_service.dart';
 import '../services/chat_service.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
